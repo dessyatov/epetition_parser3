4238b9e7911060b89e7e7dbc4697f959a2682736
dessyatov <dessyatovarthur@mail.ru>
2024-08-02 17:10:04 +0500

    Epetition parser

--- main.py
+++ main.py
@@ -1,63 +1,76 @@
+from sqlalchemy import create_engine, MetaData, Table, Column, String, Integer, select
+from sqlalchemy.orm import sessionmaker, Session
 import asyncio
-import psycopg2
-from psycopg2 import sql
 import aiohttp
 from telegram import Bot
+import os
+import dotenv
 
-db_params = {
-    'dbname': 'postgres',
-    'user': 'postgres',
-    'password': '1234',
-    'host': 'localhost',
-    'port': '5433'
-}
-telegram_token = "6585545379:AAGaPRfB-dow1quO2OXWbuWILxzoRWZtHZY"
-channel_id = "-1002149273980"
+
+dotenv.load_dotenv()
+database_url = os.getenv("DATABASE_URL")
+telegram_token = os.getenv("TOKEN")
+channel_id = os.getenv('CHANNEL')
 bot = Bot(token=telegram_token)
 
-async def create_data_base():
+
+engine = create_engine(database_url)
+metadata = MetaData()
+session = sessionmaker(bind=engine)
+
+
+petitions_table = Table(
+    'petitions_table', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('title', String(200), nullable=False),
+    Column('apply_date', String(200), nullable=False),
+    Column('signatures_number', Integer(), nullable=False),
+    Column('link', String(200), default=None)
+)
+
+class Petition:
+    def __init__(self, title, apply_date, signatures_number, link):
+        self.title = title
+        self.apply_date = apply_date
+        self.signatures_number = signatures_number
+        self.link = link
+
+def create_database():
     try:
-        conn = psycopg2.connect(**db_params)
-        conn.autocommit = True
-        cur = conn.cursor()
-        # cur.execute("CREATE DATABASE Petitions")
-        print("Database - success")
+        metadata.create_all(engine)
+        session.commit()
+        print("Database and table created successfully.")
+    except Exception as e:
+        print(f"Error creating database or table: {e}")
 
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS petitions (
-                id SERIAL PRIMARY KEY,
-                title VARCHAR(255) NOT NULL,
-                apply_date VARCHAR(255) NOT NULL,
-                signatures_number INTEGER NOT NULL,
-                link TEXT NOT NULL
-            )
-        """)
-        print("Table - success")
 
-    except psycopg2.Error as e:
-        print(f"Error: {e}")
 
-    finally:
-        cur.close()
-        conn.close()
-
-async def add_row_to_db(title, apply_date, num_of_signatures, link):
+def clear_table():
     try:
-        conn = psycopg2.connect(**db_params)
-        conn.autocommit = True
-        cur = conn.cursor()
-        cur.execute(
-            "INSERT INTO petitions (title, apply_date, signatures_number, link) VALUES (%s, %s, %s, %s)",
-            (title, apply_date, num_of_signatures, link)
+        with engine.connect() as conn:
+            conn.execute(petitions_table.delete())
+            print("Table cleared successfully.")
+    except Exception as e:
+        print(f"Error clearing table: {e}")
+
+
+
+
+def add_row_to_db(petition):
+    try:
+        ins = petitions_table.insert().values(
+            title=petition.title,
+            apply_date=petition.apply_date,
+            signatures_number=petition.signatures_number,
+            link=petition.link
         )
-        print("Data has been added successfully")
+        with engine.connect() as conn:
+            conn.execute(ins)
+            conn.commit()
+        print("Row was added to db")
+    except Exception as e:
+        print(f"Error adding row to database: {e}")
 
-    except psycopg2.Error as e:
-        print(f"Error: {e}")
-
-    finally:
-        cur.close()
-        conn.close()
 
 async def get_first_title():
     url = "https://epetition.kz/api/public/v1/petitions/short?size=21&page=0"
@@ -66,38 +79,47 @@ async def get_first_title():
             if response.status == 200:
                 data = await response.json()
                 first_title = data['content'][0]['title']
+                print("First title from website was received")
+                print(first_title)
                 return first_title
             else:
                 print("Failed to fetch data from API")
                 return None
 
-async def check_if_petition_new(title):
-    first_title_from_db = await get_first_title_from_db()
-    if title == first_title_from_db:
-        return False
-    else:
-        return True
 
-async def get_first_title_from_db():
+def check_if_petition_new(title):
     try:
-        conn = psycopg2.connect(**db_params)
-        conn.autocommit = True
-        cur = conn.cursor()
-        cur.execute("SELECT title FROM petitions ORDER BY id DESC LIMIT 1;")
-        first_row = cur.fetchone()
-        if first_row:
-            first_title = first_row[0]
+        first_title_from_db = get_first_title_from_db()
+        if first_title_from_db == title:
+            print("Petition is not new")
+            return False
         else:
-            first_title = None
-        return first_title
-
-    except psycopg2.Error as e:
-        print(f"Error: {e}")
+            print("Petition is new")
+            return True
+    except Exception as e:
+        print(f"Error checking if petition is new: {e}")
         return None
 
-    finally:
-        cur.close()
-        conn.close()
+
+def get_first_title_from_db():
+    try:
+        stmt = select(petitions_table.c.title).order_by(petitions_table.c.id.desc()).limit(1)
+        with engine.connect() as conn:
+            result = conn.execute(stmt)
+            row = result.fetchone()
+            if row:
+                print("First title from db was received")
+                print(row[0])
+
+                return row[0]
+
+            else:
+                print("First title from db was not received")
+            return None
+    except Exception as e:
+        print(f"Error fetching first title from database: {e}")
+        return None
+
 
 async def get_data_about_petition():
     url = "https://epetition.kz/api/public/v1/petitions/short?size=21&page=0"
@@ -110,41 +132,33 @@ async def get_data_about_petition():
                 apply_date = content['applyDate'][:10]
                 id = content['id']
                 title = content['title']
-                data_about_petition = {
-                    'id': id,
-                    'title': title,
-                    'apply_date': apply_date,
-                    'signatures_number': signatures_number
-                }
-                return data_about_petition
+                link = f"https://epetition.kz/petition/{id}?commentPage=0"
+                petition = Petition(title, apply_date, signatures_number, link)
+                return petition
             else:
                 print("Failed to fetch petition data")
                 return None
 
-async def print_data_about_petition(data_about_petition):
+
+async def print_data_about_petition(petition):
     message = (
         "Появилась новая петиция.\n"
-        f"Название: {data_about_petition['title']}\n"
-        f"Дата публикации: {data_about_petition['apply_date'][:10]}\n"
-        f"Количество подписей: {data_about_petition['signatures_number']}\n"
-        f"Ссылка: https://epetition.kz/petition/{data_about_petition['id']}?commentPage=0"
+        f"Название: {petition.title}\n"
+        f"Дата публикации: {petition.apply_date}\n"
+        f"Количество подписей: {petition.signatures_number}\n"
+        f"Ссылка: {petition.link}"
     )
     await bot.send_message(chat_id=channel_id, text=message)
 
 async def job():
     first_title_from_website = await get_first_title()
     if first_title_from_website:
-        is_petition_new = await check_if_petition_new(first_title_from_website)
+        is_petition_new = check_if_petition_new(first_title_from_website)
         if is_petition_new:
-            data_about_petition = await get_data_about_petition()
-            if data_about_petition:
-                await add_row_to_db(
-                    data_about_petition['title'],
-                    data_about_petition['apply_date'],
-                    data_about_petition['signatures_number'],
-                    f'https://epetition.kz/petition/{data_about_petition["id"]}?commentPage=0'
-                )
-                await print_data_about_petition(data_about_petition)
+            petition = await get_data_about_petition()
+            if petition:
+                add_row_to_db(petition)
+                await print_data_about_petition(petition)
             else:
                 print("Failed to get petition details")
         else:
@@ -152,29 +166,11 @@ async def job():
     else:
         print("Failed to get the first title")
 
-def clear_the_table():
-    try:
-        conn = psycopg2.connect(**db_params)
-        conn.autocommit = True
-        cur = conn.cursor()
-        cur.execute('DELETE FROM petitions')
-        print("Data has been deleted successfully")
-
-    except psycopg2.Error as e:
-        print(f"Error: {e}")
-
-    finally:
-        cur.close()
-        conn.close()
 
 async def main():
-    await create_data_base()
     while True:
         await job()
         await asyncio.sleep(3600)
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-
